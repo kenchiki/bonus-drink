d9515c2d638dbb9439d15ae0bf4213ed54ad127c
Kenji Nagai <info@nagai-galaxy.com>
2018-07-31 15:30:09 +0900

    [update]solve assignment

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  describe 'total_count_for' do
+    it 'when buy 0 will be 0' do
+      expect(BonusDrink.total_count_for(0)).to eq 0
+    end
+
+    it 'when buy 1 will be 1' do
+      expect(BonusDrink.total_count_for(1)).to eq 1
+    end
+
+    it 'when buy 3 will be 4' do
+      expect(BonusDrink.total_count_for(3)).to eq 4
+    end
+
+    it 'when buy 11 will be 16' do
+      expect(BonusDrink.total_count_for(11)).to eq 16
+    end
+
+    it 'when buy 100 will be 149' do
+      expect(BonusDrink.total_count_for(100)).to eq 149
+    end
+  end
 end
