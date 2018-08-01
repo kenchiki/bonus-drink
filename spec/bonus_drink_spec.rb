b43941eea1d725b23341a510c681dfeb9550cba3
Kenji Nagai <info@nagai-galaxy.com>
2018-08-01 17:08:07 +0900

    [update]refactoring

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,24 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  describe 'total_count_for' do
-    it 'when buy 0 will be 0' do
+  describe '#total_count_for' do
+    it 'total is 0, when buy 0 drinks' do
       expect(BonusDrink.total_count_for(0)).to eq 0
     end
 
-    it 'when buy 1 will be 1' do
+    it 'total is 1, when buy 1 drinks' do
       expect(BonusDrink.total_count_for(1)).to eq 1
     end
 
-    it 'when buy 3 will be 4' do
+    it 'total is 4, when buy 3 drinks' do
       expect(BonusDrink.total_count_for(3)).to eq 4
     end
 
-    it 'when buy 11 will be 16' do
+    it 'total is 16, when buy 11 drinks' do
       expect(BonusDrink.total_count_for(11)).to eq 16
     end
 
-    it 'when buy 100 will be 149' do
+    it 'total is 149, when buy 100 drinks' do
       expect(BonusDrink.total_count_for(100)).to eq 149
     end
   end
